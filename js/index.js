be6dc82bddab7ef30325dbe03ab4eee2179ff6b2
Sophie ELKHRISSI <veille.dev@gmail.com>
2020-07-12 09:54:43 +0200

    affichage html page produit

--- js/index.js
+++ js/index.js
@@ -1,26 +1,27 @@
 fetch("http://localhost:3000/api/cameras").then(response =>{
     return response.json()
 }).then(result =>{
-    //création variables pour afichage images
+    //création variables pour affichage images
     let nb_image= (result).length;
     let index_image = 0;
-        //tant que    
-        while (index_image<nb_image){
-            //Creation de la balise ou sont insérer les images
-            let balise_img = document.createElement("p");
-            // on récupére l'index de l'image dans un code html
-            balise_img.innerHTML= result [index_image].name+'<img src ='+result [index_image].imageUrl+'>';
-            // on insére le code précédent dans la div produit
-            let photo_produit = document.getElementById('produit');
-            photo_produit.appendChild(balise_img);
-                index_image++;       
 
-
-
+    //tant que    
+    while (index_image<nb_image){
+         //Creation de la balise ou sont insérer les images
+        let balise_img = document.createElement("li");
             
+        // on récupére l'index de l'image dans code index.html pour l 'afficher
+        balise_img.innerHTML= result[index_image].name+'<a href="produit.html?index='+index_image+'"> <img src ='+result [index_image].imageUrl+'></a>';
+            
+        // on insére le code précédent dans la balise liste produit
+        let photo_produit = document.getElementById('produit');           
+        if (photo_produit !== null ){photo_produit.appendChild(balise_img);}
+        
+        index_image++;    
 
 
-}
+    };
+
 })
 
 
---
+++ js/produit.js
@@ -0,0 +1,36 @@
+fetch("http://localhost:3000/api/cameras").then(response =>{
+    return response.json()
+}).then(result =>{
+    //on récupére la valeur ID de l'URL
+    let adresse_url = window.location;
+    let url = new URL(adresse_url);
+    let index_produit = url.searchParams.get("index");
+    
+    //On crée un élément article
+    let produit = document.createElement("article");
+            
+    // on récupére l'index de l'image dans code produit.html pour l 'afficher
+    produit.innerHTML= '<img src='+result[index_produit].imageUrl+'>';
+    produit.innerHTML+='<p class=description>'+result[index_produit].description +'</p>';
+    
+    //séparateur nombre
+    let prix = result[index_produit].price;
+    produit.innerHTML+='<p class=prix>'+prix.toLocaleString('fr') +' € </p>';
+    
+    // on insére le code précédent dans la balise div produit
+    let photo_produit = document.getElementById('produit');           
+    if (photo_produit !== null ){photo_produit.appendChild(produit);}
+
+    console.log(result[0].imageUrl)
+    
+
+
+    
+
+    
+
+
+    
+    
+    
+})
