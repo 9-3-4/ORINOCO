7bc31ce1db0012ed626dbf3b5b673b1133cef023
9-3-4 <veille.dev@gmail.com>
2020-08-16 19:42:03 +0200

    refonte site via _id

--- js/description.js
+++ js/description.js
@@ -1,74 +1,50 @@
-//fonction pour rÃ©cupÃ©reration de l'url en synchrone
-function lire_fichier_json(file, callback) {
-    var fichier_json = new XMLHttpRequest();
-    fichier_json.overrideMimeType("application/json");
-    fichier_json.open("GET", file, true);
-    fichier_json.onreadystatechange = function() {
-        if (fichier_json.readyState === 4 && fichier_json.status == "200") {
-            callback(fichier_json.responseText);
-        }
-    }
-    fichier_json.send(null);
+//récupération de la valeur de l'option lentille selectionner pour l'afficher dans l'url
+function passer_commande(id_produit) {
+    choix_valider = document.getElementsByClassName("description_option");
+    console.log(choix_valider[0].value);
+    var option_lentille = choix_valider[0].value;
+    window.location = 'panier.html?id_produit=' + id_produit + '&option=' + option_lentille;
+    
 }
-//enregistrement des options 
-function selection_option_lentille(choix_option){
-    localStorage.setItem('option', choix_option);
-}
-//pour appareil avec une seule option ou sans avoir selectionner une option
-function passer_commande(){
-    if (typeof selection_choix == 'undefined'){
-       selection_choix = "25mm 4.5"; 
-    }else{
-        option_choisi = document.getElementById("selection_option").selectedIndex;
-        selection_choix = (document.getElementsByTagName("option")[option_choisi]).value;
-        console.log(selection_choix);
-    }
-   
-    if(selection_choix=='Selectionner une lentille'){
-        alert("Oups ! Vous avez oubliÃ© de sÃ©lectionner l'option.");
-    }else{
-        commande =[produit_selectionne,selection_choix];
-        localStorage.setItem(Date.now(),JSON.stringify(commande));
-        window.location = "panier.html";
 
-    }
-}
-let produit_selectionne = sessionStorage.getItem('produit-selectionne');
-let description = document.getElementById('produit');
-let url_api = "http://localhost:3000/api/cameras";
+//récupération de l'url avec une seule donnée, l'ID
+id_produit = (window.location.search.substr(1).split('id_produit='))[1];
 
-//si produit selectionner il s'affiche
-if (produit_selectionne !== null){ 
-    let titre_produit_description =document.getElementById("titre_description");
-    lire_fichier_json(url_api, function(text){
-    	
-    let donnee_json = JSON.parse(text); 
-        titre_produit_description.innerHTML ='DÃ©tail de l\'appareil '+donnee_json[produit_selectionne].name;  
-    let description_camera =  
-        '<div id="img_description"><img src="'+donnee_json[produit_selectionne].imageUrl+'" width="395px" height="263px" > </div> \
-        <div id="texte_description"><h2>'+donnee_json[produit_selectionne].name+'</h2> \
-        <p>' +donnee_json[produit_selectionne].description+'</p>';
-        description_camera_footer = '<footer id="bouton"><button class="btn_ajouter_panier" onclick="passer_commande()">Commander</button></footer></div>';
-    let option_lentilles = (donnee_json[produit_selectionne]).lenses;
-        description.innerHTML = '<h1> DÃ©tail de l\'appareil '+donnee_json[produit_selectionne].name+'</h1>';
+//lecture du fichier json qui envoie la promesse dans variable id_produit
+fetch("http://localhost:3000/api/cameras")
+    .then(response => {
 
-//affichage option tant qu'il y en a dans une liste deroulante
-if (option_lentilles.length > 1){
-    let  description_option_lentille ='';
-        for (var option_lentille in option_lentilles){
-            description_option_lentille += '<option  value="'+option_lentilles[option_lentille]+'">'+option_lentilles[option_lentille]+'</option>';
-            }; 
-            description.innerHTML = '<article id="article_description">'+description_camera + '<fieldset><legend>Lentilles disponibles </legend><select id="selection_option"><option>Selectionner une lentille</option>'+description_option_lentille +'</select></fieldset>'+ description_camera_footer +'</article>';        
+            fichier_json = response.json();
+            var liste_produit = Promise.resolve(fichier_json);
 
-//si une seule option affichera seulement l'option sans liste dÃ©roulante
-}else{
-    description.innerHTML = '<article id="article_description">'+description_camera + '<p class="lentille_sans_option">Lentille disponible : '+option_lentilles+'</p>'+ description_camera_footer +'</article>';
-    }  
+            //Création du tableau de produit dans div description
+            var description = document.getElementById('description');
 
-});
+            //afficher seulement la valeur du tableau (array du json)
+            liste_produit.then((valeur) => {
 
-//si aucun article n'a Ã©tÃ© choisie, message    
-}else{
-     description.innerHTML = '<article id="article_description_manquante"><p><img src="./images/pas_darticle.png"><span>Oups ! il semblerait que vous n\'ayez pas choisi d\'article ! <a href="index.html">Consulter notre catalogue en ligne</a></span></p></article>';
+                //trouver dans les valeurs la ligne id_produit
+                var information_appareil_photo = valeur.find(appareil_photo => appareil_photo._id === id_produit);
+                
+                //Récupération des valeurs du tableau des options dans une variable
+                var liste_option_lentille = (information_appareil_photo.lenses).values();
+                
+                
+                //Creation de la balise Select pour créer une liste déroulant
+                liste_deroulante_option = '<select class="description_option"><option>Selectionner une lentille</option>' ;
+                
+                //Récupération de toutes les valeurs pour les mettre dans une balise option de la liste deroulante
+                for (var valeur_option of liste_option_lentille) {
+                    liste_deroulante_option += "<option value='" + valeur_option +"'>" + valeur_option + "</option>";
+                    }
 
-}
+                //Fermeture de la balise select
+                liste_deroulante_option += '</select>';
+                                              
+                // Création du code HTML
+                description.innerHTML = '<article><h2 class="description_name">' + information_appareil_photo.name + '</h2><div class="description_photo"><img src="' + information_appareil_photo.imageUrl + '"></div><div class="description_selectionne"><p>' + information_appareil_photo.description + '</p></div>' + liste_deroulante_option + '<p class="description_prix"> ' + information_appareil_photo.price + ' &#x20AC </p> <button class="btn_ajouter_panier" onclick="passer_commande(\''+id_produit+'\')">Commander</button> </artticle > ';
+                              
+            });
+
+    });//fin fetch
+;
---
+++ js/main.js
@@ -0,0 +1,22 @@
+//lecture du fichier json qui envoie la promesse dans variable liste_produit
+function redirect_description_produit(id_produit) {
+    window.location = 'description.html?id_produit='+ id_produit;
+}
+//pour lire le fichier json
+fetch("http://localhost:3000/api/cameras")
+    .then(response => {
+        
+        fichier_json = response.json();
+        var liste_produit = Promise.resolve(fichier_json);
+        console.log(liste_produit);
+
+        //Création du tableau de produit dans div catalogue
+        var catalogue = document.getElementById('catalogue')
+
+        //Parcourir le tableau de json et afficher en html ce que l'on veut
+        liste_produit.then((valeur) => {
+            valeur.forEach(appareil_photo =>
+                catalogue.innerHTML += '<article class="article_catalogue"><div class="photo_article"> <img src="' + appareil_photo.imageUrl + '"onclick="redirect_description_produit(\'' + appareil_photo._id +'\')" ></div><h2 class="index_name">' + appareil_photo.name + '</h2></div><button class="index_ensavoirplus" onclick="redirect_description_produit(\'' + appareil_photo._id+'\')" >En savoir +</button></article>');
+                        
+        });
+    });//fin fetch
