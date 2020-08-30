4805c263c0b15e17e4c0a4f47f42214532a21c83
9-3-4 <veille.dev@gmail.com>
2020-08-30 22:31:34 +0200

    correction diverse

--- js/confirmation.js
+++ js/confirmation.js
@@ -11,7 +11,7 @@ var confirmation = document.getElementById('confirmation');
 //création du Html
 confirmation.innerHTML = `<article>
                              <h2 class="hello">Bonjour ${prenom_client} ${nom_client},</h2>
-                             <p class="remerciement">Toutes notre &#233quipe vous remercie et vous informe que votre commande <span class="bold"> ${numero_commande_client} </span> d'un montant de <span class="bold"> ${parseInt(totaux_commande).toLocaleString('fr-FR')} &#x20AC </span>  est valid&#233e.</p>
+                             <p class="remerciement">Toute notre &#233quipe vous remercie et vous informe que votre commande <span class="bold"> ${numero_commande_client} </span> d'un montant de <span class="bold"> ${parseInt(totaux_commande).toLocaleString('fr-FR')} &#x20AC </span>  est valid&#233e.</p>
                              <p class="livraison">Votre colis vous sera livr&#233 comme pr&#233vu sous 48h.</p>
                              <img class="chat" src="./images/livraison.png">
                           </article > `;
